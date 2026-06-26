023e44db3b8a34e9038006fe5cd528529b599947
Jedrick Ocenar <jdpocenar@mymail.mapua.edu.ph>
2026-06-26 10:02:27 +0800

    added construct cache.py for easier cache-ig

---
+++ constructCache.py
@@ -0,0 +1,8 @@
+from services.rag import EmbeddingRetriever
+
+for corpus in ["data/raw_text/waray_text.jsonl",
+               "data/raw_text/cebuano_text.jsonl",
+               "data/raw_text/hiligaynon_text.jsonl"]:
+    EmbeddingRetriever(corpus_path=corpus, rebuild=False)
+
+print("✅ Embedding complete!")
